a5a12178507efa9260d98d709e6fbd485482c05c
Yoshiyuki Sugiyama <yo.sugiyama67@gmail.com>
2021-02-16 08:36:58 +0900

    feat: Add YenCount program.

---
+++ YenCount/yen_count.py
@@ -0,0 +1,20 @@
+YEN_TYPES = [10000, 5000, 1000, 500, 100, 50, 10, 1]
+
+def getYenCount(money):
+    count = [0] * len(YEN_TYPES)
+    for i in range(0, len(YEN_TYPES)):
+        while money >= YEN_TYPES[i]:
+            money -= YEN_TYPES[i]
+            count[i] += 1
+    return count
+
+if __name__ == '__main__':
+    money = input("金額を入力してください。>>> ")
+    try:
+        money = int(money)
+    except ValueError:
+        print("数値を入力してください。")
+
+    count = getYenCount(money)
+    for i in range(0, len(YEN_TYPES)):
+        print(str(YEN_TYPES[i]) + "円\t" + str(count[i]) + "枚")
